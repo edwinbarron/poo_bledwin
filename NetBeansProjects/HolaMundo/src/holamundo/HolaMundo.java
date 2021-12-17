f404b9d28a051626ee63a2401995315db98c0552
edwinbarron <ea.barron.l@edu.utc.mx>
2021-12-17 12:33:23 -0600

    libreria

---
+++ NetBeansProjects/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola Mundo");
+    }
+    
+    
+}
